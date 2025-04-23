a223c9a3e1047de6fd6c41b47b7e33c173e7e436
Nicholas <nick.makmak@gmail.com>
2025-04-23 14:23:39 -0400

    question1 solution

---
+++ src/nick/Question_1.java
@@ -0,0 +1,25 @@
+package nick;
+
+public class Question_1 {
+
+    public static void main(String[] args) {
+
+        identify(8);
+        identify(5);
+
+    }
+    public static void identify (int number){
+        if (number % 2 == 0 ){
+            System.out.println("Number " + number + " is even.");
+        } else {
+            System.out.println("Number " + number + " is odd.");
+        }
+    }
+}
+/**
+ * Odd or Even
+ * Write  a method which can identify given number is even or odd
+ *          Output ex:
+ *          identify(5); ->"Odd"
+ *          identify(6); ->"Even"
+ */
