d6234971155d5f4962240049a6a8177f0d53dbdf
sunisaservais <sunisaservais@gmaill.com>
2025-04-23 13:42:31 -0400

    Completed

--- src/sunisa/Question_1.java
+++ src/sunisa/Question_1.java
@@ -18,10 +18,10 @@ public class Question_1 {
     }
 }
 
-/**
- * Odd or Even
- * Write  a method which can identify given number is even or odd
- * Output ex:
- * identify(5); ->"Odd"
- * identify(6); ->"Even"
+/*
+ Odd or Even
+ Write  a method which can identify given number is even or odd
+ Output ex:
+ identify(5); ->"Odd"
+ identify(6); ->"Even"
  */
