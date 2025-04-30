4b260fa87ceb7ee35ad564223da536a66ae974a6
StefanSukur <sukurescu@gmail.com>
2025-04-29 22:14:56 -0400

    both tasks completed

---
+++ src/stefan/Question_2.java
@@ -0,0 +1,22 @@
+package stefan;
+
+public class Question_2 {
+    public static void main(String[] args) {
+        finra();
+    }
+
+
+    public static void finra() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
