6154a4bedcb1a17eb57e1d21d79e23c8f06d1c59
Haden <haden@cydeo.com>
2025-05-05 21:29:05 +0300

    3rd Question published

---
+++ src/hayden/Question_3.java
@@ -0,0 +1,12 @@
+package hayden;
+
+public class Question_3 {
+
+    public static void main(String[] args) {
+
+
+        /**
+         Swap two variables' values without using a third variable
+         */
+    }
+}
