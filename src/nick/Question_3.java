2b66ae506dca30dee01914a78a3ab8e652b31bff
Nicholas <nick.makmak@gmail.com>
2025-05-06 11:46:53 -0400

    Question 3 is done!

---
+++ src/nick/Question_3.java
@@ -0,0 +1,16 @@
+package nick;
+
+public class Question_3 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+
+        a = a + b; // a = 15
+        b = a - b; // b = 5
+        a = a - b; // a = 10
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
