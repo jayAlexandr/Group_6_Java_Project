dd903be6109fd4bb989c40b81f80647902afec35
ijuraev <juraeviskandar.11@gmail.com>
2025-08-05 15:53:17 -0400

    Task 10 finished

---
+++ src/iskandar/Question_10.java
@@ -0,0 +1,31 @@
+package iskandar;
+
+public class Question_10 {
+
+    public static void main(String[]  args){
+
+        int[] arr = {99, 12, 23, 32, 44, 57, 6};
+
+        System.out.println("minNumber(arr) = " + minNumber(arr));
+
+    }
+
+    public static int minNumber(int[] arrayNums){
+
+        int result = arrayNums[0];
+
+        for (int i = 0; i < arrayNums.length; i++) {
+
+            if (arrayNums[i] < result){
+
+                result = arrayNums[i];
+            }
+        }
+        return result;
+    }
+
+    /*
+         Write a method that can find the minimum number from an int Array
+         Ex:  int[] arr = {99, 12, 23, 32, 44, 57, 6} ==> 6
+         */
+}
